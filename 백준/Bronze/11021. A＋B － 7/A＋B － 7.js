fdd5e456e41ba983655f99065709936a0a2c75e6
GuDoYoon(구도윤) <154503723+rhehfl@users.noreply.github.com>
2025-02-27 12:04:19 +0900

    [Bronze V] Title: A+B - 7, Time: 88 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,14 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '예제.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+let result = '';
+
+for (let i = 1; i <= +input[0]; i++) {
+  const [a, b] = input[i].split(' ');
+  result += `Case #${i}: ${+a + +b}\n`;
+}
+
+console.log(result);
