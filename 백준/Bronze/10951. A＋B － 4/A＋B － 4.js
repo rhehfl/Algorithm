f2928b199ce9b01476695bae20fb0c78a86591a7
GuDoYoon(구도윤) <154503723+rhehfl@users.noreply.github.com>
2025-03-02 15:31:41 +0900

    [Bronze V] Title: A+B - 4, Time: 96 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,15 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '예제.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+let result = '';
+
+for (let i = 0; i < input.length; i++) {
+  const [a, b] = input[i].split(' ').map(Number);
+  const sum = a + b;
+  result += sum + '\n';
+}
+
+console.log(result);
