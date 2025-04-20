1649223accf98b720dce9e858e30fd3de7e1372d
GuDoYoon(구도윤) <154503723+rhehfl@users.noreply.github.com>
2025-04-20 15:26:55 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 88 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -2,10 +2,11 @@ const input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '예제.txt')
   .toString();
 
+const memo = {};
 function fibo(n) {
   if (n === 0 || n === 1) return n;
-
-  return fibo(n - 1) + fibo(n - 2);
+  if (memo[n] !== undefined) return memo[n];
+  memo[n] = fibo(n - 1) + fibo(n - 2);
+  return memo[n];
 }
-
 console.log(fibo(Number(input)));
