bc3ae6fc01c6ad540eba6133dc52e233abbcd1fe
GuDoYoon(구도윤) <154503723+rhehfl@users.noreply.github.com>
2025-04-22 12:55:47 +0900

    [Bronze II] Title: Tautogram, Time: 196 ms, Memory: 12188 KB -BaekjoonHub

--- 백준/Bronze/5698. Tautogram/Tautogram.js
+++ 백준/Bronze/5698. Tautogram/Tautogram.js
@@ -8,9 +8,14 @@ for (let i = 0; i < input.length - 1; i++) {
   if (line === '*') break; // 안전하게 처리
   console.log(isTautogram(line) ? 'Y' : 'N');
 }
-
 function isTautogram(str) {
-  const words = str.split(/\s+/); // 공백 하나 이상으로 split
-  const firstChar = words[0][0].toLowerCase();
-  return words.every(word => word[0].toLowerCase() === firstChar);
+  const strArray = str.split(/\s+/);
+  const firstChar = strArray[0][0].toLowerCase();
+  let bool = true;
+  strArray.forEach((str) => {
+    if (str[0].toLowerCase() !== firstChar) {
+      bool = false;
+    }
+  });
+  return bool;
 }
