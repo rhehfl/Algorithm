be703a253c868bda4b737290632e833362ea25ef
GuDoYoon(구도윤) <154503723+rhehfl@users.noreply.github.com>
2025-05-12 18:47:57 +0900

    [Gold IV] Title: N-Queen, Time: 7192 ms, Memory: 10184 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.js
+++ 백준/Gold/9663. N－Queen/N－Queen.js
@@ -5,30 +5,32 @@ const N = Number(
     .trim()
 );
 
-const board = Array.from({ length: N }, () => null);
+const board = Array(N).fill(false);
 let count = 0;
-
 function dfs(row) {
   if (row === N) {
     count++;
     return;
   }
-  for (let column = 0; column < N; column++) {
-    if (checker(row, column)) {
-      board[row] = column;
+
+  for (let i = 0; i < N; i++) {
+    if (checker(row, i, board)) {
+      board[row] = i;
       dfs(row + 1);
       board[row] = null;
     }
   }
 }
 
-function checker(row, col) {
+function checker(row, col, board) {
   for (let i = 0; i < row; i++) {
+    //같은 열에 있는지 검사
     if (board[i] === col) return false;
+    //같은 대각선상에 있는지 검사
     if (Math.abs(i - row) === Math.abs(board[i] - col)) return false;
   }
   return true;
 }
-
 dfs(0);
+
 console.log(count);
