44dbfcf3dfc5d201c17d78aa1e5214317b58ecca
GuDoYoon(구도윤) <154503723+rhehfl@users.noreply.github.com>
2025-05-16 17:39:52 +0900

    [Silver III] Title: 파도반 수열, Time: 88 ms, Memory: 9628 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -0,0 +1,27 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '예제.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const P = [];
+let result = '';
+P[1] = 1;
+P[2] = 1;
+P[3] = 1;
+
+for (let i = 1; i <= input[0]; i++) {
+  const num = Number(input[i]);
+  result += `${padoban(num)}\n`;
+}
+
+console.log(result);
+
+function padoban(n) {
+  if (P[n]) return P[n];
+  for (let i = 4; i <= n; i++) {
+    P[i] = P[i - 2] + P[i - 3];
+  }
+  return P[n];
+}
+console.log();
