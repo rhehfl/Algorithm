947c07e30b892c249e393cbd87244650f15a138f
GuDoYoon(구도윤) <154503723+rhehfl@users.noreply.github.com>
2025-09-09 14:57:59 +0900

    [Silver I] Title: 접두사 찾기, Time: 436 ms, Memory: 39532 KB -BaekjoonHub

---
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : 'GDY/baekjoon/input.txt';
+const input = fs.readFileSync(filePath, 'utf8').trim().split('\n');
+
+const [N, M] = input[0].trim().split(' ').map(Number);
+
+class Node {
+  constructor() {
+    this.children = new Map();
+    this.isEnd = false;
+  }
+}
+class Trie {
+  constructor() {
+    this.head = new Node();
+  }
+
+  insert(word) {
+    let node = this.head;
+
+    for (const char of word) {
+      if (!node.children.has(char)) {
+        node.children.set(char, new Node());
+      }
+      node = node.children.get(char);
+    }
+    node.isEnd = true;
+  }
+
+  searchPrefix(prefix) {
+    let node = this.head;
+    for (const char of prefix) {
+      if (!node.children.has(char)) return false;
+      node = node.children.get(char);
+    }
+    return true;
+  }
+}
+const prefixes = [];
+const trie = new Trie();
+let prefixCount = 0;
+
+for (let i = 1; i <= N; i++) {
+  trie.insert(input[i].trim());
+}
+
+for (let i = 1; i <= M; i++) {
+  if (trie.searchPrefix(input[i + N].trim())) prefixCount++;
+}
+
+console.log(prefixCount);
